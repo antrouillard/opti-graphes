00f571b2fa0428c7a6ad94e6900892f8ec149ce0
antrouillard <antoine.rouillard@outlook.fr>
2025-03-13 08:13:04 +0100

    feat: basic file reading

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ import os
 
 def read_file(path : str):
     
+    with open(path, "r", encoding="utf-8") as fichier:
+        for ligne in fichier:
+            print(ligne)
 
+
+def main():
+    instance1_txt_path="Instances/Instance1.txt"
+    read_file(instance1_txt_path)
     
+main()
