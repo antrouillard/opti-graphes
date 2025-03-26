be63c8b0cb136b1b9ea26287418738cd7db99f73
Antho76 <62425937+Antho76@users.noreply.github.com>
2025-03-26 10:22:42 +0100

    Récupération infos dans un fichier Ros

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 # Developped by Antoine Rouillard, Thomas Vauley, Adam Kaoukeb and Anthonin Pain
 import classes
 import os
+import xml.etree.ElementTree as ET
 
-def read_file(path : str):
-    
-    with open(path, "r", encoding="utf-8") as fichier:
-        for ligne in fichier:
-            print(ligne)
+tree = ET.parse("Instances/Instance1.ros")
+root = tree.getroot()
 
 
 
+print(f"Racine : {root.tag}")
+""" for element in root.iter():
+    print(f"{element.tag} : {element.text}")"""
 
-def main():
-    instance1_txt_path="Instances/Instance1.txt"
-    read_file(instance1_txt_path)
-    
-main()
+#exemple de recupération d'un élément, ici Contract 
+
+contracts = {}
+for contract in root.find("Contracts"):
+    contract_id = contract.get("ID")
+    contracts[contract_id] = {
+        "MinRestTime": contract.find("MinRestTime").text if contract.find("MinRestTime") is not None else None,
+        "MaxSeq": [seq.attrib for seq in contract.findall("MaxSeq")],
+        "MinSeq": [seq.attrib for seq in contract.findall("MinSeq")],
+        "Workload": {
+            "Max": contract.find("Workload/TimeUnits/Max/Count").text if contract.find("Workload/TimeUnits/Max/Count") is not None else None,
+            "Min": contract.find("Workload/TimeUnits/Min/Count").text if contract.find("Workload/TimeUnits/Min/Count") is not None else None,
+        },
+        "ValidShifts": contract.find("ValidShifts").get("shift") if contract.find("ValidShifts") is not None else None
+    }
+
+# Affichage des contrats
+for contract_id, details in contracts.items():
+    print(f"Contract ID: {contract_id}")
+    print(details)
+    print("-" * 30)
+
